11a75bae998fb59095d3c83b7f92262b6702dcb6
Spoc Web <10584012+SpocWeb@users.noreply.github.com>
2022-08-13 15:02:01 +0200

    compiles but does not start

--- CarvedRock.Api/Startup.cs
+++ CarvedRock.Api/Startup.cs
@@ -5,9 +5,11 @@ using CarvedRock.Api.GraphQL;
 using CarvedRock.Api.Repositories;
 using GraphQL;
 using GraphQL.Execution;
+using GraphQL.NewtonsoftJson;
 using GraphQL.Server;
 using GraphQL.Server.Authorization.AspNetCore;
 using GraphQL.Server.Ui.Playground;
+using GraphQL.SystemTextJson;
 using GraphQL.Validation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -18,65 +20,66 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace CarvedRock.Api
 {
-    public class Startup
-    {
-        private readonly IConfiguration _config;
-        private readonly IHostingEnvironment _env;
+	public class Startup
+	{
+		private readonly IConfiguration _config;
+		private readonly IHostingEnvironment _env;
 
-        public Startup(IConfiguration config, IHostingEnvironment env)
-        {
-            _config = config;
-            _env = env;
-        }
+		public Startup(IConfiguration config, IHostingEnvironment env)
+		{
+			_config = config;
+			_env = env;
+		}
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<CarvedRockDbContext>(options =>
-                options.UseSqlServer(_config["ConnectionStrings:CarvedRock"]));
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<CarvedRockDbContext>(options =>
+				options.UseSqlServer(_config["ConnectionStrings:CarvedRock"]));
 
-            services.AddScoped<ProductRepository>();
-            services.AddScoped<ProductReviewRepository>();
+			services.AddScoped<ProductRepository>();
+			services.AddScoped<ProductReviewRepository>();
 
-            services.AddScoped<IServiceProvider>(s => new FuncServiceProvider (s.GetRequiredService));
-            services.AddScoped<CarvedRockSchema>();
-            services.AddSingleton<ReviewMessageService>();
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			services.AddScoped<IServiceProvider>(s => new FuncServiceProvider(s.GetRequiredService));
+			services.AddScoped<CarvedRockSchema>();
+			services.AddSingleton<ReviewMessageService>();
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
-            // https://stackoverflow.com/questions/53537521/how-to-implement-authorization-using-graphql-net-at-resolver-function-level
-            services.AddTransient<IValidationRule, AuthorizationValidationRule>()
-	        .AddAuthorization(options =>
-	        {
-	            options.AddPolicy("LoggedIn", p => p.RequireAuthenticatedUser());
-	        });
-            services.AddGraphQL(o => o.ExposeExceptions = _env.IsDevelopment())
-                .AddGraphTypes(ServiceLifetime.Scoped)
-                //.AddUserContextBuilder(ContextCreator)
-                .AddDataLoader()
-                .AddWebSockets();
+			services.AddScoped<IGraphQLSerializer, GraphQLSerializer>();
 
-            services.AddCors();
-        }
+			// https://stackoverflow.com/questions/53537521/how-to-implement-authorization-using-graphql-net-at-resolver-function-level
+			services.AddTransient<IValidationRule, AuthorizationValidationRule>()
+				.AddAuthorization(options => { options.AddPolicy("LoggedIn", p => p.RequireAuthenticatedUser()); });
 
-        static ClaimsPrincipal ContextCreator(HttpContext httpContext)
-        {
-	        return httpContext.User;
-        }
+			// Extensions from graphql.server.core\5.2.2
+			services.AddGraphQL(o => o.UnhandledExceptionDelegate = HandleException)
+				; /*
+				.AddGraphTypes(ServiceLifetime.Scoped)
+				//.AddUserContextBuilder(ContextCreator)
+				.AddDataLoader();
+				//.AddWebSockets();
+*/
+		}
 
-        static void HandleException(UnhandledExceptionContext ex)
-        {
-	        Console.Error.WriteLine(ex.ErrorMessage);
-	        ex.Exception = null!;
-        }
+		static ClaimsPrincipal ContextCreator(HttpContext httpContext)
+		{
+			return httpContext.User;
+		}
 
-        public void Configure(IApplicationBuilder app, CarvedRockDbContext dbContext)
-        {
-            app.UseCors(builder =>
-                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
-            app.UseWebSockets();
-            app.UseGraphQLWebSockets<CarvedRockSchema>("/graphql");
-            app.UseGraphQL<CarvedRockSchema>();
-            app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
-            dbContext.Seed();
-        }
-    }
+		static void HandleException(UnhandledExceptionContext ex)
+		{
+			Console.Error.WriteLine(ex.ErrorMessage);
+			ex.Exception = null!;
+		}
+
+		public void Configure(IApplicationBuilder app, CarvedRockDbContext dbContext)
+		{
+			app.UseCors(builder =>
+				builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+			app.UseWebSockets();
+			//app.UseGraphQLWebSockets<CarvedRockSchema>("/graphql");
+			//app.UseGraphQL<CarvedRockSchema>();
+			app.UseGraphQLPlayground(new PlaygroundOptions());
+			dbContext.Seed();
+		}
+	}
 }
